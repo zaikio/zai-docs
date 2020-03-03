13cc8362d761548f5b98321715801ba90366b22c
Martin Spickermann <spickermann@gmail.com>
2020-03-03 10:11:41 +0100

    Add Procurement (Suppliers) API docs

--- scripts/fetchDocs.js
+++ scripts/fetchDocs.js
@@ -12,7 +12,8 @@ async function asyncForEach(array, callback) {
 
 const AVAILABLE_APPS = {
   directory: "https://directory.zaikio.com",
-  loom: "https://loom.zaikio.com"
+  loom: "https://loom.zaikio.com",
+  procurement_suppliers: "http://procurement.zaikio.test/suppliers"
 };
 
 const navFilePath = path.join(
