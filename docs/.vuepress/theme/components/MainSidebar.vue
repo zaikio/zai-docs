dffe2aca86806c17f664ba4f8c0927b729f14936
Jalyna <jalyna.schroeder@gmail.com>
2020-12-07 15:55:16 +0100

    Update style links

--- docs/.vuepress/theme/components/MainSidebar.vue
+++ docs/.vuepress/theme/components/MainSidebar.vue
@@ -11,17 +11,19 @@
 
     <div v-if="currentApp">
       <RouterLink to="/">← Home</RouterLink>
-      <div class="app-box">
-        <div
-          v-if="currentApp.logo"
-          class="app-box__logo"
-          :style="`background-image:url(${currentApp.logo});`"
-        ></div>
-        <h3>{{ currentApp.title }}</h3>
+      <div class="app-section">
+        <div class="app-box">
+          <div
+            v-if="currentApp.logo"
+            class="app-box__logo"
+            :style="`background-image:url(${currentApp.logo});`"
+          ></div>
+          <h3>{{ currentApp.title }}</h3>
+        </div>
+        <p v-if="currentApp.summary">
+          <small>{{ currentApp.summary }}</small>
+        </p>
       </div>
-      <p v-if="currentApp.summary">
-        <small>{{ currentApp.summary }}</small>
-      </p>
 
       <SidebarLinks
         :depth="0"
@@ -29,19 +31,21 @@
         :items="toItems(currentApp.guides)"
       />
 
-      <h3>Reference</h3>
-      <SidebarLinks
-        :depth="0"
-        :sidebar-depth="0"
-        :items="
-          Object.keys(currentApp.references).map(label => ({
-            path: currentApp.references[label],
-            title: label,
-            type: 'group',
-            children: []
-          }))
-        "
-      />
+      <div class="app-references">
+        <h3>Reference</h3>
+        <SidebarLinks
+          :depth="0"
+          :sidebar-depth="0"
+          :items="
+            Object.keys(currentApp.references).map(label => ({
+              path: currentApp.references[label],
+              title: label,
+              type: 'group',
+              children: []
+            }))
+          "
+        />
+      </div>
     </div>
 
     <div v-if="!currentApp">
@@ -181,16 +185,24 @@
                   children: []
                 }
               ]
-            },
+            }
           ]"
         />
       </div>
 
-      <h3>Apps</h3>
-      <ul class="sidebar-links">
+      <h3>Zaikio Apps</h3>
+      <ul class="sidebar-links app-list">
         <li v-for="item in apps">
           <div class="depth-0">
-            <RouterLink :to="item.home">{{ item.title }}</RouterLink>
+            <RouterLink :to="item.home">
+              <div
+                v-if="item.logo"
+                class="link-app-logo"
+                :style="`background-image:url(${item.logo});`"
+              ></div>
+              <div v-if="!item.logo" class="link-app-logo"></div>
+              <div class="link-app-text">{{ item.title }}</div>
+            </RouterLink>
           </div>
         </li>
       </ul>
@@ -277,12 +289,13 @@ export default {
 
 .app-box__logo
   width: 40px
+  min-width: 40px
   height: 40px
   background-size: cover
   background-repeat: no-repeat
   float: left
   flex-basis: 40px
-  margin-right: 10px
+  margin-right: 20px
 
 .sidebar-group.depth-0 > a + .sidebar-links
   display: none
@@ -302,6 +315,69 @@ export default {
 .sidebar-group.depth-0 > a.router-link-active + .sidebar-links
 //.sidebar-group.depth-0 > a.open + .sidebar-links
   display: block
+
+.app-list.sidebar-links
+  .link-app-logo
+    height: 40px
+    width: 40px
+    background-size: cover
+    background-color: #ccc
+    border-radius: 6px
+    min-width: 40px
+    flex-basis: 40px
+    margin-right: 20px
+
+  li:nth-of-type(4n - 3) .depth-0 > a
+    .link-app-logo
+      background-color: #E2B3FF
+
+  li:nth-of-type(4n - 2) .depth-0 > a
+    .link-app-logo
+      background-color: #B3C4FF
+
+  li:nth-of-type(4n - 1) .depth-0 > a
+    .link-app-logo
+      background-color: #FFE380
+
+  li:nth-of-type(4n - 0) .depth-0 > a
+    .link-app-logo
+      background-color: #FFBD99
+
+  li .depth-0 > a
+    display: flex
+    align-items: center
+    padding-left: 0
+    padding-right: 0
+
+.app-section
+  margin-bottom: 40px
+
+  p
+    line-height: 1.5
+
+.app-references
+  margin-top: 40px
+
+  .sidebar-links li .depth-0 > a
+    display: block
+    background-image: url(./book_inactive.svg)
+    background-repeat: no-repeat
+    background-position: 12px center
+    padding: 4px 12px 4px 48px
+    color: lighten($textColor, 60%)
+    background-size: 20px
+    border-radius: 6px
+    line-height: 1.7
+
+    &:hover
+      background-image: url(./book_hover.svg)
+      color: #fff
+
+    &.active
+      background-image: url(./book_active.svg)
+      color: $textColor
+      background-color: $accentColorLight
+
 
 .home-link
   display: block
@@ -370,5 +446,4 @@ export default {
         &.active
           color: $white
           font-weight: 600
-
 </style>
