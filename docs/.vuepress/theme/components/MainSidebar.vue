ae5c0e7ae0fd234ca66e4f13fc7a195d844c97e1
Jalyna <jalyna.schroeder@gmail.com>
2020-12-07 14:04:08 +0100

    Enhance link styles in sidebar

--- docs/.vuepress/theme/components/MainSidebar.vue
+++ docs/.vuepress/theme/components/MainSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="zaikio-sidebar">
-    <RouterLink :to="$localePath" class="home-link">
+    <RouterLink :to="$localePath" class="home-link-logo">
       <img
         v-if="$site.themeConfig.logo"
         class="logo"
@@ -45,146 +45,146 @@
     </div>
 
     <div v-if="!currentApp">
-      <SidebarLinks
-        :depth="0"
-        :sidebar-depth="0"
-        :items="[
-          {
-            path: '/',
-            title: 'Home',
-            type: 'group',
-            children: []
-          },
-          {
-            path: '/getting-started/',
-            title: 'Getting started',
-            type: 'group',
-            children: [
-              {
-                path: '/getting-started/create-app.html',
-                title: '1. Setup Zaikio App',
-                type: 'group',
-                children: []
-              },
-              {
-                path: '/getting-started/sso-person.html',
-                title: '2. SSO with Zaikio account',
-                type: 'group',
-                children: []
-              },
-              {
-                path: '/getting-started/use-api.html',
-                title: '3. Fetch data with API',
-                type: 'group',
-                children: []
-              },
-              {
-                path: '/getting-started/organisation-install.html',
-                title: '4. Install app for organisations',
-                type: 'group',
-                children: []
-              }
-            ]
-          },
-          {
-            path: '/changelog/',
-            title: 'Changelog',
-            type: 'group',
-            children: []
-          },
-          {
-            title: 'Guides',
-            path: '/guide/oauth/',
-            type: 'group',
-            children: [
-              {
-                path: '/guide/oauth/',
-                title: 'OAuth',
-                type: 'group',
-                children: [
-                  {
-                    path: '/guide/oauth/redirect-flow',
-                    title: 'Redirect flow',
-                    children: []
-                  },
-                  {
-                    path: '/guide/oauth/device-flow',
-                    title: 'Device Flow',
-                    children: []
-                  },
-                  {
-                    path: '/guide/oauth/client-credentials',
-                    title: 'Client Credentials',
-                    children: []
-                  },
-                  {
-                    path: '/guide/oauth/access-token-refresh',
-                    title: 'Access token refresh',
-                    children: []
-                  },
-                  {
-                    path: '/guide/oauth/delegate-access',
-                    title: 'Delegate Access',
-                    children: []
-                  },
-                  {
-                    path: '/guide/oauth/scopes',
-                    title: 'Scopes',
-                    children: []
-                  }
-                ]
-              },
-              {
-                path: '/guide/try-api/',
-                title: 'Try Zaikio APIs',
-                type: 'group',
-                children: []
-              },
-              {
-                path: '/guide/loom/',
-                title: 'Event Handling with Loom',
-                type: 'group',
-                children: [
-                  {
-                    path: '/guide/loom/receiving-events',
-                    title: 'Receiving events',
-                    children: []
-                  },
-                  {
-                    path: '/guide/loom/posting-events',
-                    title: 'Posting events',
-                    children: []
-                  }
-                ]
-              },
-              {
-                path: '/guide/provide-api/',
-                title: 'Provide API and events',
-                type: 'group',
-                children: []
-              },
-              {
-                path: '/guide/jwt/',
-                title: 'JWT Validation',
-                type: 'group',
-                children: []
-              },
-              {
-                path: '/guide/launchpad/',
-                title: 'Launchpad',
-                type: 'group',
-                children: []
-              },
-              {
-                path: '/guide/migrate-existing-customers/',
-                title: 'Migrate an existing Customer Database',
-                type: 'group',
-                children: []
-              }
-            ]
-          },
-        ]"
-      />
+      <RouterLink :to="$localePath" class="home-link">
+        Home
+      </RouterLink>
+
+      <div class="guide-links">
+        <SidebarLinks
+          :depth="0"
+          :sidebar-depth="0"
+          :items="[
+            {
+              path: '/getting-started/',
+              title: 'Getting started',
+              type: 'group',
+              children: [
+                {
+                  path: '/getting-started/create-app.html',
+                  title: '1. Setup Zaikio App',
+                  type: 'group',
+                  children: []
+                },
+                {
+                  path: '/getting-started/sso-person.html',
+                  title: '2. SSO with Zaikio account',
+                  type: 'group',
+                  children: []
+                },
+                {
+                  path: '/getting-started/use-api.html',
+                  title: '3. Fetch data with API',
+                  type: 'group',
+                  children: []
+                },
+                {
+                  path: '/getting-started/organisation-install.html',
+                  title: '4. Install app for organisations',
+                  type: 'group',
+                  children: []
+                }
+              ]
+            },
+            {
+              path: '/changelog/',
+              title: 'Changelog',
+              type: 'group',
+              children: []
+            },
+            {
+              title: 'Guides',
+              path: '/guide/oauth/',
+              type: 'group',
+              children: [
+                {
+                  path: '/guide/oauth/',
+                  title: 'OAuth',
+                  type: 'group',
+                  children: [
+                    {
+                      path: '/guide/oauth/redirect-flow',
+                      title: 'Redirect flow',
+                      children: []
+                    },
+                    {
+                      path: '/guide/oauth/device-flow',
+                      title: 'Device Flow',
+                      children: []
+                    },
+                    {
+                      path: '/guide/oauth/client-credentials',
+                      title: 'Client Credentials',
+                      children: []
+                    },
+                    {
+                      path: '/guide/oauth/access-token-refresh',
+                      title: 'Access token refresh',
+                      children: []
+                    },
+                    {
+                      path: '/guide/oauth/delegate-access',
+                      title: 'Delegate Access',
+                      children: []
+                    },
+                    {
+                      path: '/guide/oauth/scopes',
+                      title: 'Scopes',
+                      children: []
+                    }
+                  ]
+                },
+                {
+                  path: '/guide/try-api/',
+                  title: 'Try Zaikio APIs',
+                  type: 'group',
+                  children: []
+                },
+                {
+                  path: '/guide/loom/',
+                  title: 'Event Handling with Loom',
+                  type: 'group',
+                  children: [
+                    {
+                      path: '/guide/loom/receiving-events',
+                      title: 'Receiving events',
+                      children: []
+                    },
+                    {
+                      path: '/guide/loom/posting-events',
+                      title: 'Posting events',
+                      children: []
+                    }
+                  ]
+                },
+                {
+                  path: '/guide/provide-api/',
+                  title: 'Provide API and events',
+                  type: 'group',
+                  children: []
+                },
+                {
+                  path: '/guide/jwt/',
+                  title: 'JWT Validation',
+                  type: 'group',
+                  children: []
+                },
+                {
+                  path: '/guide/launchpad/',
+                  title: 'Launchpad',
+                  type: 'group',
+                  children: []
+                },
+                {
+                  path: '/guide/migrate-existing-customers/',
+                  title: 'Migrate an existing Customer Database',
+                  type: 'group',
+                  children: []
+                }
+              ]
+            },
+          ]"
+        />
+      </div>
 
       <h3>Apps</h3>
       <ul class="sidebar-links">
@@ -254,13 +254,13 @@ export default {
   padding: 2rem
   position: fixed
   overflow-y: auto
-  background-color: $greySidebarBg 
+  background-color: $greySidebarBg
   color: $white
 
 .logo
   max-width: 100%
 
-.home-link
+.home-link-logo
   display: block
   margin-bottom: 4rem
 
@@ -294,9 +294,33 @@ export default {
     border: none
     border-radius: 6px
 
-.sidebar-group.depth-0 > a.router-link-active + .sidebar-links,
-.sidebar-group.depth-0 > a.open + .sidebar-links
+.sidebar-group.depth-0 > a.router-link-active + .sidebar-links
+//.sidebar-group.depth-0 > a.open + .sidebar-links
   display: block
+
+.home-link
+  display: block
+  margin-bottom: 40px
+  background-image: url(./home_inactive.svg)
+  background-repeat: no-repeat
+  background-position: 12px center
+  padding: 4px 12px 4px 48px
+  color: lighten($textColor, 60%)
+  background-size: 20px
+  border-radius: 6px
+  line-height: 1.7
+
+  &:hover
+    background-image: url(./home_hover.svg)
+    color: #fff
+
+  &.router-link-exact-active
+    background-image: url(./home_active.svg)
+    color: $textColor
+    background-color: $accentColorLight
+
+.guide-links
+  margin-bottom: 40px
 
 .sidebar-links
   list-style: none
@@ -306,14 +330,18 @@ export default {
   li
     margin: 0 0 8px 0
     padding: 0
+
     a
+      font-size: 1em
       display: block
       color: lighten($textColor, 60%)
       border: none
       border-radius: 6px
+      padding: 4px 12px
+      font-weight: 600;
 
     a:hover
-      color: $textColor
+      color: $accentColor
       // background-color: $greySidebarBgLighter
 
     a.router-link-exact-active
