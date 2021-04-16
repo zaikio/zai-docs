07d98923eba2fb046a032c6da59a8ccd815c54de
schmoko <krone21@gmail.com>
2021-04-16 11:52:45 +0200

    add matomo tracking

---
+++ docs/.vuepress/matomo-snippet.js
@@ -0,0 +1,11 @@
+var _paq = window._paq = window._paq || [];
+_paq.push(['disableCookies']);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://zaikio.matomo.cloud/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '4']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/zaikio.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+})();
